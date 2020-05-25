832122f16a61910aae44c66f0b15648f9f4bb385
hugokipje <45482407+hugokipje@users.noreply.github.com>
2020-05-25 12:33:04 +0200

    rotorshift

--- main.py
+++ main.py
@@ -2,3 +2,14 @@ alfabet = "abcdefghijklmnopqrstuvwxyz"
 rotorA = alfabet
 rotorB = alfabet
 rotorC = alfabet
+
+
+def shift(tekst, amount):
+  amount = amount % 26  
+  eersteDeel = tekst[amount:26]
+  tweedeDeel = tekst[0:amount]
+  return eersteDeel + tweedeDeel
+
+print(shift(alfabet,9))
+print(shift(alfabet,20))
+print(shift(alfabet,23))
